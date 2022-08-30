9db0a1ee40cfa089baca82fa7b30af7a580a2311
flora <lihua.cheng@aispeech.com>
2022-08-30 19:05:22 +0800

    接口关联参数化
    Signed-off-by: flora <lihua.cheng@aispeech.com>

--- common/request_util.py
+++ common/request_util.py
@@ -3,37 +3,34 @@ import requests
 from common.text_util import *
 
 
+def assert_util(expect,response,run_result_txt):
+
+    print(response)
+    for expect_word in expect.split(","):
+        assertion = expect_word in response
+        if assertion:
+            assert assertion
+            # 将运行结果写入txt文件保存
+            write_txt(text_file=run_result_txt, data=response+"__pass|\n")  # 用"__"符号间隔                    
+        else:
+            assert assertion
+            write_txt(text_file=run_result_txt, data=response+"__fail|\n")        
+
+
 def request_util(method, url, headers, payloads=None, params=None, expect=None, run_result_txt=None):
     if method == 'get':
         res = requests.request('GET',url, headers=headers, params=params)
-        print(res.text)
-        for expect_word in expect.split(","):
-            assertion = expect_word in res.text
-            if assertion:
-                assert assertion
-                # 将运行结果写入txt文件保存
-                write_txt(text_file=run_result_txt, data=res.text+"__pass|")  # 用"__"符号间隔                    
-            else:
-                assert assertion
-                write_txt(text_file=run_result_txt, data=res.text+"__fail|")        
-        return res.text
 
     elif method == 'post':
         res = requests.request('POST', url=url, headers=headers, data=payloads)
-        print(res.text)
-        for expect_word in expect.split(","):
-            assertion = expect_word in res.text
-            # print("断言是：", assertion)
-            if assertion:
-                assert assertion
-                write_txt(text_file=run_result_txt, data=res.text+"__pass|")
-            else:
-                assert assertion
-                write_txt(text_file=run_result_txt, data=res.text+"__fail|")
-        return res.text
+    
     elif method == 'put':
-        pass
+        res = requests.request('PUT', url=url, headers=headers, data=payloads) 
     
     elif method == 'delete':
-        pass
+        res = requests.request('DELETE', url=url, headers=headers, params=params) 
+
+
+    assert_util(expect, res.text, run_result_txt)
+    return res.text
 
--- testcase/test_users.py
+++ testcase/test_users.py
@@ -8,19 +8,18 @@ import json
 #from common.log_util import LogUtil
 
 
-
-@pytest.mark.run(order=1)
+@pytest.mark.users
 @pytest.mark.all
 @pytest.mark.parametrize('args', extract_util('%s/data/yaml/users.yaml' % base_dir))
 def test_users(args):
     url = args['url']
     header = eval(args['header'])
     method = args['method']
-    payload = json.dumps(eval(args['body']))
+    payloads = json.dumps(eval(args['body']))
     params = eval(args['body'])
     expect = args['expect_result']  # 断言依据
     run_result_txt = '%s/data/run_result.txt' % base_dir
 
-    rep = request_util(method, url, headers=header, payloads=payload, params=params, expect=expect, run_result_txt=run_result_txt)
+    rep = request_util(method, url, headers=header, payloads=payloads, params=params, expect=expect, run_result_txt=run_result_txt)
     print("测试返回:",rep)
 
